8ac4a167de5313887b64ac4ae758c9d0464e9d56
Frithjof Siebecke <fsiebecke@gmail.com>
2024-10-06 19:38:14 +0200

    material auf catalog version umgestellt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,9 +58,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//   implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
     implementation(libs.androidx.appcompat)  // AppCompat hinzufügen
@@ -77,8 +74,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-messaging-ktx:23.0.0")
 
-    implementation("com.google.android.material:material:1.12.0")
-//    implementation("androidx.fragment:fragment:1.8.4")
+    implementation(libs.material)
 
     // silent login verschlüsselung
     implementation(libs.bcprov.jdk15on) // Für Bouncy Castle (z. B. AES)
